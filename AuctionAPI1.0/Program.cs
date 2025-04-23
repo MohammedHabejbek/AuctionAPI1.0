a62b04e7710d926fb07353f33b3d1834c987ed9e
Mohammed Habejbek <Mohamed.Habej@outlook.com>
2025-04-23 08:31:47 +0200

    Configured CORS to allow cross-origin requests in the future

--- AuctionAPI1.0/Program.cs
+++ AuctionAPI1.0/Program.cs
@@ -35,7 +35,16 @@ builder.Services.AddScoped<AuctionAPI1._0.Repositories.IBidRepository, AuctionAP
 builder.Services.AddScoped<JwtService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -49,8 +58,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
+
 app.UseAuthentication();
 
 app.MapControllers();
